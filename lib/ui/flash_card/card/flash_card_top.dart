660ba030a89da11b6eae9be451e5c0883c1d6018
John Ryan Murphy <johnryan@themurphycompany.com>
2021-05-04 18:46:41 +0800

    ui
    - top box hide new

--- lib/ui/flash_card/card/flash_card_top.dart
+++ lib/ui/flash_card/card/flash_card_top.dart
@@ -20,23 +20,30 @@ class FlashCardTopState extends State<FlashCardTop> {
 
   @override
   Widget build(BuildContext context) {
-    if (_currentColor == null) _currentColor = Color(0x00FFFFFF);
+    if (_currentColor == null) _currentColor = Color(0xFFBCBCBC);
     if (_currentText == null) _currentText = "";
     return Container(
-        alignment: Alignment.center,
         decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [_currentColor.withOpacity(0.1), _currentColor],
-            ),
+            color: Colors.white.withOpacity(_currentText != "" ? 1.0 : 0.0),
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(10.0),
               topRight: Radius.circular(10.0),
             )),
-        width: widget.width,
-        height: widget.height / 10,
-        child: Text(_currentText, style: normalResponsiveFont(context)));
+        child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color(0x00BCBCBC), _currentColor],
+                ),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0),
+                )),
+            width: widget.width,
+            height: widget.height / 10,
+            child: Text(_currentText, style: normalResponsiveFont(context))));
   }
 
   void updateTab(Color color, String text) {
